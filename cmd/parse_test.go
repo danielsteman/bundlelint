9edb4a09c3a1f71d3d2f8de70f8207c8b8df22dc
Daniel Steman <daniel-steman@live.nl>
2025-01-13 22:17:23 +0100

    add test

--- cmd/parse_test.go
+++ cmd/parse_test.go
@@ -25,13 +25,25 @@ func TestParseLintConfig(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Failed to parse bundle config: %v", err)
 	}
-    if config.NotificationsInProd != true {
+	if config.NotificationsInProd != true {
 		t.Fatalf("Expected value to be true: %v", err)
-    }
+	}
 	prettyConfig, err := json.MarshalIndent(config, "", "  ")
 	if err != nil {
 		log.Fatalf("Failed to marshal config: %v", err)
 	}
 
 	fmt.Println("Config:", string(prettyConfig))
+}
+
+func TestParseLintConfigMissingTool(t *testing.T) {
+	_, err := ParseLintConfig("../test_bundle/pyproject_no_tools.toml")
+	if err == nil {
+		t.Fatalf("Expected an error due to missing [tool] section, but got none")
+	}
+
+	expectedError := "[tool.bundlelint] section not found in package config"
+	if err.Error() != expectedError {
+		t.Fatalf("Unexpected error message. Got: %v, want: %v", err.Error(), expectedError)
+	}
 }
