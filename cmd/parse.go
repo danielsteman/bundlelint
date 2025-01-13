c3b2c1b61945aa403360c3965a42179b2ec02d68
Daniel Steman <daniel-steman@live.nl>
2025-01-13 22:05:21 +0100

    refactor

--- cmd/parse.go
+++ cmd/parse.go
@@ -9,15 +9,15 @@ import (
 )
 
 type BundleName struct {
-	name string
+	Name string `yaml:"name"`
 }
 
 type BundleConfig struct {
-	bundle BundleName
+	Bundle BundleName `yaml:"bundle"`
 }
 
 type LintConfig struct {
-	notifications_in_prod bool
+	NotificationsInProd bool `toml:"notifications_in_prod"`
 }
 
 func ParseBundleConfig(path string) (*BundleConfig, error) {
@@ -26,14 +26,10 @@ func ParseBundleConfig(path string) (*BundleConfig, error) {
 		return nil, fmt.Errorf("failed to read bundle config: %w", err)
 	}
 
-	fmt.Println(data)
-
 	var config BundleConfig
 	if err := yaml.Unmarshal(data, &config); err != nil {
 		return nil, fmt.Errorf("failed to parse bundle config: %w", err)
 	}
-
-	fmt.Println(config)
 
 	return &config, nil
 }
@@ -44,33 +40,20 @@ func ParseLintConfig(path string) (*LintConfig, error) {
 		return nil, fmt.Errorf("failed to read package config: %w", err)
 	}
 
-	var config map[string]interface{}
+	var config struct {
+		Tool struct {
+			BundleLint *LintConfig `toml:"bundlelint"`
+		} `toml:"tool"`
+	}
+
 	if err := toml.Unmarshal(data, &config); err != nil {
 		return nil, fmt.Errorf("failed to parse package config: %w", err)
 	}
 
-	tool, ok := config["tool"].(map[string]interface{})
-	if !ok {
-		return nil, fmt.Errorf("[tool] section not found in package config")
-	}
-
-	bundlelint, ok := tool["bundlelint"].(map[string]interface{})
-	if !ok {
+	if config.Tool.BundleLint == nil {
 		return nil, fmt.Errorf("[tool.bundlelint] section not found in package config")
 	}
 
-	var lintConfig LintConfig
-	if err := mapToStruct(bundlelint, &lintConfig); err != nil {
-		return nil, fmt.Errorf("failed to parse [tool.bundlelint] section: %w", err)
-	}
-
-	return &lintConfig, nil
+	return config.Tool.BundleLint, nil
 }
 
-func mapToStruct(input map[string]interface{}, output interface{}) error {
-	serialized, err := yaml.Marshal(input)
-	if err != nil {
-		return err
-	}
-	return yaml.Unmarshal(serialized, output)
-}
--- cmd/parse_test.go
+++ cmd/parse_test.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"testing"
 )
 
@@ -10,5 +12,26 @@ func TestParseBundleConfig(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Failed to parse bundle config: %v", err)
 	}
-	fmt.Println(config)
+	prettyConfig, err := json.MarshalIndent(config, "", "  ")
+	if err != nil {
+		log.Fatalf("Failed to marshal config: %v", err)
+	}
+
+	fmt.Println("Config:", string(prettyConfig))
+}
+
+func TestParseLintConfig(t *testing.T) {
+	config, err := ParseLintConfig("../test_bundle/pyproject.toml")
+	if err != nil {
+		t.Fatalf("Failed to parse bundle config: %v", err)
+	}
+    if config.NotificationsInProd != true {
+		t.Fatalf("Expected value to be true: %v", err)
+    }
+	prettyConfig, err := json.MarshalIndent(config, "", "  ")
+	if err != nil {
+		log.Fatalf("Failed to marshal config: %v", err)
+	}
+
+	fmt.Println("Config:", string(prettyConfig))
 }
